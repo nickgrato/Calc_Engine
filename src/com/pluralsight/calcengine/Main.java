7c504d3a150ad05379336103ba49db453e1f0487
Nick Grato <ngrato@gmail.com>
2017-10-13 09:16:06 -0700

    adding switch statment and an array

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,28 +3,40 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double val1 = 100.0d;
-        double val2 = 0.0d;
-        double result;
-        char opCode = 'd';
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
-        if(opCode == 'a'){
-            result = val1 + val2;
-        }
-        else if(opCode == 's'){
-            result = val1 - val2;
-        }
-        else if(opCode == 'd'){
-            result = val2 != 0.0d ? val1 / val2 : 0.0d;
-        }
-        else if(opCode == 'm'){
-            result = val1 * val2;
-        }
-        else{
-            System.out.print("Error - invalid opCode ");
-            result = 0;
+
+        for (int i = 0; i < opCodes.length; i++) {
+                switch (opCodes[i]){
+                    case 'a':
+                        results[i] = leftVals[i] + rightVals[i];
+                        break;
+                    case 's':
+                        results[i] = leftVals[i] - rightVals[i];
+                        break;
+                    case 'd':
+                        results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+                        break;
+                    case 'm':
+                        results[i] = leftVals[i] * rightVals[i];
+                        break;
+                    default:
+                        System.out.print("Error - invalid opCode ");
+                        results[i] = 0.0d;
+                }
+            }
+
+        for (double theResult : results) {
+            System.out.print("result = ");
+            System.out.println(theResult);
         }
 
-        System.out.print("result = " + result );
+
+        }
+
+
     }
-}
+
